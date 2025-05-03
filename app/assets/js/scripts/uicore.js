0477ffca8b01ca412c5067e81881885fcdf44d9d
emykr <bravegirl0904@gmail.com>
2025-05-03 14:07:21 +0900

    fix: 업데이트 UI의 상태를 false로 변경하고 한국어 메시지 수정 feat: 버전 번호를 1.0.20으로 업데이트

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -107,7 +107,7 @@ function changeAllowPrerelease(val){
 
 function showUpdateUI(info){
     //TODO Make this message a bit more informative `${info.version}`
-    document.getElementById('image_seal_container').setAttribute('update', true)
+    document.getElementById('image_seal_container').setAttribute('update', false)
     document.getElementById('image_seal_container').onclick = () => {
         /*setOverlayContent('Update Available', 'A new update for the launcher is available. Would you like to install now?', 'Install', 'Later')
         setOverlayHandler(() => {
